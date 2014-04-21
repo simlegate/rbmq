2fdbbadc00b3752fefe2eddbc7a3095c284dd800
都是牛奶装什么特仑苏丶 <simlegate@163.com>
2014-04-21 10:35:01 +0800

    mv starting server to cli

--- lib/rbmq/cli.rb
+++ lib/rbmq/cli.rb
@@ -1,11 +1,29 @@
 require 'thor'
 module Rbmq
   class CLI < Thor
-    map "-v" => :version
+    map "-s" => :server, "-v" => :version
+
+    method_option :host, :aliases => "-h", :desc => "Default: 0.0.0.0"
+    method_option :port, :type => :numeric, :aliases => "-p", :desc => "Default: 61613"
+
+    desc "server", "Serve your rbmq"
+    def server host="0.0.0.0", port=61613
+      EventMachine.run do
+        Rbmq::QueueManager.current = Rbmq::QueueManager.new(Rbmq::Queue::Memory::Runner.new)
+        EventMachine.start_server(host, port, Rbmq::Server::EventMachine)
+
+        Signal.trap("INT")  { EventMachine.stop }
+        Signal.trap("TERM") { EventMachine.stop }
+      end
+    end
 
     desc "version", "Display Rbmq gem version"
     def version
       puts Rbmq::VERSION
+    end
+
+    desc "doctor", "doctor"
+    def doctor
     end
   end
 end
